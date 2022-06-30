1bdf440bff4e99ae5a801ddeb567eb4379a7bb6e
VickyA5 <vavalos@fi.uba.ar>
2022-06-29 23:47:43 -0300

    una llave

--- menu.cpp
+++ menu.cpp
@@ -358,9 +358,10 @@ void Menu::listar_lecturas_por_escritor(Lista_lecturas& l_lecturas,
     int i = 1;
     while (i <= l_lecturas.obtener_cantidad()) {
         if (l_lecturas.consulta(i)->obtener_autor()
-            && respuesta == stoi(l_lecturas.consulta(i)->obtener_autor()->obtener_codigo()))
+            && respuesta == stoi(l_lecturas.consulta(i)->obtener_autor()->obtener_codigo())){
             cout << endl;
             l_lecturas.consulta(i)->mostrar_datos();
+        }
         i++; 
     }
 }
